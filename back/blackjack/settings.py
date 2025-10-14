3ab4e43781669847bdd3c8bddf41be1d1c1cfec0
Gabriel-Penteadoo <gabrielpenteado131@gmail.com>
2025-10-14 15:25:19 +0200

    added game_manager with a working test game

--- back/blackjack/settings.py
+++ back/blackjack/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'game_manager.apps.GameManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.game_manager',
+    
     
 ]
 
--- back/game_manager/models.py
+++ back/game_manager/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=250)
+    turn = models.IntegerField(default=0)
+    ended = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    # Exemple de méthode personnalisée
+    def start_game(self):
+        """Démarre la partie"""
+        self.turn = 1
+        self.ended = False
+        self.save()
+
+    @classmethod
+    def get_game(cls, game_id):
+        """Retourne une partie spécifique par ID"""
+        return cls.objects.filter(id=game_id).first()
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    score = models.IntegerField(default=0)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='players')
+    stand = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} (Game: {self.game.name})"
